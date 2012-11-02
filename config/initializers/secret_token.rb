97a85e306c38e0ed6e37baef83b7946f91aded91
Brian Kung <draklyne@gmail.com>
2012-11-01 23:36:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collide::Application.config.secret_token = '452ca14e69f871e68cafcf4b793bcc8c71fb2d571589676edab14edc7859c792e07d2877bf217cf060d0089cd415e64ef763d9096898a9888c14c09835e00c86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Collide::Application.config.session_store :cookie_store, key: '_collide_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Collide::Application.config.session_store :active_record_store
