dcbddd04bdc084f4fa072dc0b4e0f4ca015248c6
Brian Kung <draklyne@gmail.com>
2012-11-04 21:52:04 -0600

    Adds bootstrap

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -2,6 +2,6 @@ class Concept < ActiveRecord::Base
   attr_accessible :description, :name, :url
 
   has_many :implementations
-  has_many :votes, as: :voteable, dependent: :destroy
+  has_many :votes, as: :voteable, :dependent => :destroy
   belongs_to :hero
 end
